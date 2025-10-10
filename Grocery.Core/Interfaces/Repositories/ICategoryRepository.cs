33bbe8b39be3b997e6259c1d5f70e87616584102
Edward Popova <S1204881@student.windesheim.nl>
2025-10-10 11:40:05 +0200

    Interface en Repository voor Category aangemaakt.

---
+++ Grocery.Core/Interfaces/Repositories/ICategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using Grocery.Core.Models;
+
+namespace Grocery.Core.Interfaces.Repositories
+{
+    public interface ICategoryRepository
+    {
+        List<Category> GetAll(); // Geeft alle categorien terug.
+
+        Category? Get(int id); // Geeft categorie terug met unieke identificatie.
+
+        Category Add(Category item); // Maakt nieuwe categorie aan.
+
+        Category? Update(Category item); // Updates al gemaakte categorie.
+
+        Category? Delete(Category item); // Verwijderd een categorie.
+
+        bool Exists(string name); // Checkt of een categorie met hetzelfde naam al bestaat.
+    }
+}

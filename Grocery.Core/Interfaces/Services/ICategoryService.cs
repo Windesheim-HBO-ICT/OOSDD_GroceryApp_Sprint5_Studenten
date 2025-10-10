f94133683f24c549795d791a9ace623eb1d13949
Edward Popova <S1204881@student.windesheim.nl>
2025-10-10 12:37:31 +0200

    Interface en Service voor Category aangemaakt.

---
+++ Grocery.Core/Interfaces/Services/ICategoryService.cs
@@ -0,0 +1,19 @@
+﻿using Grocery.Core.Models;
+
+namespace Grocery.Core.Interfaces.Services
+{
+    public interface ICategoryService
+    {
+        List<Category> GetAll(); // Onvangt alle categorieen.
+
+        Category? Get(int id); // Ontvangt een categorie door zijn unieke indentificatie.
+
+        Category? Add(Category item); // Voegt een nieuwe categorie toe.
+
+        Category? Update(Category item); // Updates een al gemaakte categorie.
+
+        Category? Delete(Category item); //Verwijderd een categorie.
+
+        bool IsCategoryNameUnique(string name); // Valideert of de categorie naam al bestaat.
+    }
+}
---
+++ Grocery.Core/Services/CategoryService.cs
@@ -0,0 +1,57 @@
+﻿using Grocery.Core.Interfaces.Repositories;
+using Grocery.Core.Interfaces.Services;
+using Grocery.Core.Models;
+
+namespace Grocery.Core.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        // Initialiseerd een nieuwe instance van de CategoryService class.
+        public CategoryService(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        // Ontvangt alle categorieen van de repository.
+        public List<Category> GetAll()
+        {
+            return _categoryRepository.GetAll();
+        }
+
+        // Onvangt een categorie door zijn unieke identificatie.
+        public Category? Get(int id)
+        {
+            return _categoryRepository.Get(id);
+        }
+
+        // Voegt een nieuwe categorie na het valideren.
+        public Category? Add(Category item)
+        {
+            if (_categoryRepository.Exists(item.Name))
+            {
+                return null;
+            }
+            return _categoryRepository.Add(item);
+        }
+
+        // Updates een al gemaatke categorie in de repository.
+        public Category? Update(Category item)
+        {
+            return _categoryRepository.Update(item);
+        }
+
+        // Verwijderd een categorie van de repository.
+        public Category? Delete(Category item)
+        {
+            return _categoryRepository.Delete(item);
+        }
+
+        // Valideert of een categorie naam al in gebruik is.
+        public bool IsCategoryNameUnique(string name)
+        {
+            return !_categoryRepository.Exists(name);
+        }
+    }
+}
